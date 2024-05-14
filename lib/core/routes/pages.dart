b069bc4c1e0de2f06887f1348dde9149533029b0
Thanh nguyen <jonhsonle1122@gmail.com>
2024-05-14 10:12:43 +0700

    update new

--- lib/core/routes/pages.dart
+++ lib/core/routes/pages.dart
@@ -1,4 +1,6 @@
 import 'package:find_food/core/routes/routes.dart';
+import 'package:find_food/features/main/di/main_binding.dart';
+import 'package:find_food/features/main/presentation/page/main_page.dart';
 import 'package:get/get.dart';
 
 class Pages {
