b8b52d81236f393317db4fed1715d26ecff2c5ef
minnhhttris <ntmtri02@gmail.com>
2024-05-23 14:59:34 +0700

    create post model

--- lib/features/model/post_model.dart
+++ lib/features/model/post_model.dart
@@ -1,45 +1,65 @@
+import 'package:get/get.dart';
+
 class PostModel {
   final String? id;
   final String title;
   final String subtitle;
-  final double rate;
-  final List imageList;
-  final String restaurantId;
+  final RxInt favoriteCount;
+  final List<String> imageList;
+  final String? restaurantId;
   final String createAt;
+  final RxBool isBookmarked;
+  final RxBool isFavorited;
 
   PostModel({
     this.id,
     required this.title,
     required this.subtitle,
-    required this.rate,
+    int favoriteCount = 0,
     required this.imageList,
     required this.restaurantId,
     required this.createAt,
-  });
+  })  : favoriteCount = RxInt(favoriteCount),
+        isBookmarked = false.obs,
+        isFavorited = false.obs;
 
-  PostModel copyWith(
-      {String? id,
-      String? title,
-      String? subtitle,
-      double? rate,
-      List? imageList,
-      String? restaurantId,
-      String? createAt}) {
+  PostModel copyWith({
+    String? id,
+    String? title,
+    String? subtitle,
+    int? favoriteCount,
+    List<String>? imageList,
+    String? restaurantId,
+    String? createAt,
+    RxBool? isBookmarked,
+    RxBool? isFavorited,
+  }) {
     return PostModel(
-        id: id ?? this.id,
-        title: title ?? this.title,
-        subtitle: subtitle ?? this.subtitle,
-        rate: rate ?? this.rate,
-        imageList: imageList ?? this.imageList,
-        restaurantId: restaurantId ?? this.restaurantId,
-        createAt: createAt ?? this.createAt);
+      id: id ?? this.id,
+      title: title ?? this.title,
+      subtitle: subtitle ?? this.subtitle,
+      favoriteCount: favoriteCount ?? this.favoriteCount.value,
+      imageList: imageList ?? this.imageList,
+      restaurantId: restaurantId ?? this.restaurantId,
+      createAt: createAt ?? this.createAt,
+    );
+  }
+
+  void toggleFavorite() {
+    isFavorited.value = !isFavorited.value;
+    if (isFavorited.value) {
+      favoriteCount.value += 1;
+    } else {
+      favoriteCount.value -= 1;
+    }
   }
 
   Map<String, dynamic> toJson() {
     return {
+      'id': id,
       'title': title,
       'subtitle': subtitle,
-      'rate': rate,
+      'favoriteCount': favoriteCount.value,
       'imageList': imageList,
       'restaurantId': restaurantId,
       'createAt': createAt,
@@ -51,8 +71,8 @@ class PostModel {
       id: json['id'],
       title: json['title'],
       subtitle: json['subtitle'],
-      rate: json['rate'],
-      imageList: json['imageList'],
+      favoriteCount: json['favoriteCount'],
+      imageList: List<String>.from(json['imageList']),
       restaurantId: json['restaurantId'],
       createAt: json['createAt'],
     );
