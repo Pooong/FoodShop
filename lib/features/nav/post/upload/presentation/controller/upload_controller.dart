9b1ea7a90891bb89c7c4efb52a4dc048ec38a49d
minnhhttris <ntmtri02@gmail.com>
2024-05-24 16:51:49 +0700

    profile card and models

--- lib/features/nav/post/upload/presentation/controller/upload_controller.dart
+++ lib/features/nav/post/upload/presentation/controller/upload_controller.dart
@@ -1,5 +1,9 @@
 import 'dart:io';
 
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:find_food/core/data/firebase/firestore_database/firestore_post.dart';
+import 'package:find_food/core/data/firebase/model/result.dart';
+import 'package:find_food/features/model/post_model.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:image_picker/image_picker.dart';
@@ -26,8 +30,18 @@ class UploadController extends GetxController {
     selectedImages.remove(image);
   }
 
-  void uploadPost() {
-    // Implement your upload logic here
-    // You can use titleController.text, descriptionController.text, and selectedImages
+  void uploadPost() async {
+    print("==============================");
+    final result = await FirestorePost.createPost(
+      PostModel(
+        title: "dasda",
+        description: "description",
+        imageList: [],
+        restaurantId: "restaurantId",
+        createdAt: "createAt",
+        favorites: 0,
+        postId: "postId",
+      ),
+    );
   }
-}
+  }
--- lib/features/nav/profile/nav/profile_list/presentation/page/profile_list_page.dart
+++ lib/features/nav/profile/nav/profile_list/presentation/page/profile_list_page.dart
@@ -1,4 +1,4 @@
-import 'package:find_food/core/ui/widgets/card/explore_food_card.dart';
+import 'package:find_food/core/ui/widgets/card/profile_card.dart';
 import 'package:find_food/features/nav/profile/nav/profile_list/presentation/controller/profile_list_controller.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
@@ -8,16 +8,14 @@ class ProfileListPage extends GetView<ProfileListController> {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
+    return ListView.builder(
       padding: const EdgeInsets.only(left: 20, right: 10),
-      child: GridView.count(
-                        crossAxisCount: 2, // Số cột trong GridView
-                        crossAxisSpacing: 10, // Khoảng cách ngang giữa các ô
-                        mainAxisSpacing: 10, // Khoảng cách dọc giữa các ô
-                        children: [
-                          for (var i = 1;i<=20;i++) const ExploreFoodCard(),
-                        ],
-      ),
+      itemBuilder: (context, index) {
+        // return ProfileCard(
+        //   postModel: controller.postList[index],
+        // );
+      },
+      itemCount: 3,
     );
   }
 }
