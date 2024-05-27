2cc424e745cbc943744498d6e7464d1e3da87217
Pooong <thanthan101101@gmail.com>
2024-05-27 10:02:58 +0700

    edit .

--- lib/features/nav/post/upload/presentation/page/upload_page.dart
+++ lib/features/nav/post/upload/presentation/page/upload_page.dart
@@ -1,5 +1,6 @@
 import 'package:find_food/core/configs/app_colors.dart';
 import 'package:find_food/core/configs/app_dimens.dart';
+import 'package:find_food/core/routes/routes.dart';
 import 'package:find_food/core/ui/widgets/appbar/upload_post_appbar.dart';
 import 'package:find_food/core/ui/widgets/button/button_widget.dart';
 import 'package:find_food/features/nav/post/upload/presentation/controller/upload_controller.dart';
@@ -11,8 +12,6 @@ class UploadPage extends GetView<UploadController> {
 
   @override
   Widget build(BuildContext context) {
-    // final controller = Get.put(UploadController());
-
     return Scaffold(
       appBar: const UploadPostAppbar(),
       body: SingleChildScrollView(
