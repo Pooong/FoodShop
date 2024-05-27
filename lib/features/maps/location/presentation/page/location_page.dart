c3c9b01a03b616e51c6c8aa7a792fba5f89330c3
Thanh nguyen <jonhsonle1122@gmail.com>
2024-05-27 07:42:15 +0700

    update code

--- lib/features/maps/location/presentation/page/location_page.dart
+++ lib/features/maps/location/presentation/page/location_page.dart
@@ -1,6 +1,5 @@
 import 'package:find_food/core/configs/app_colors.dart';
 import 'package:find_food/core/ui/widgets/appbar/get_location_appbar.dart';
-import 'package:find_food/features/maps/location/models/place.dart';
 import 'package:find_food/features/maps/location/presentation/controller/location_controller.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
@@ -26,26 +25,20 @@ class LocationPage extends GetView<LocationController> {
                 children: [
                   GestureDetector(
                     child: const Icon(Icons.search),
-                    onTap: () {
-                      controller
-                          .handleSearch(controller.addressController.text);
-                    },
+                    onTap: () {},
                   ),
                   SizedBox(width: 10),
                   Expanded(
                       child: SizedBox(
                     height: 30,
                     child: TextField(
-                      controller: controller.addressController,
                       decoration: const InputDecoration(
                         labelText: "search location this here",
                         labelStyle:
                             TextStyle(color: Color.fromARGB(187, 0, 0, 0)),
                         border: OutlineInputBorder(borderSide: BorderSide.none),
                       ),
-                      onChanged: (value) {
-                        controller.onTextChange(value);
-                      },
+                      onChanged: (value) {},
                     ),
                   ))
                 ],
@@ -56,8 +49,7 @@ class LocationPage extends GetView<LocationController> {
               margin: const EdgeInsets.symmetric(vertical: 15),
               child: ElevatedButton(
                   onPressed: () {
-                    // Get.toNamed('/maps');
-                    controller.getCurrentPosition();
+                    Get.toNamed('/maps');
                   },
                   style: ButtonStyle(
                       backgroundColor: MaterialStateProperty.all<Color>(
@@ -73,25 +65,23 @@ class LocationPage extends GetView<LocationController> {
                     ],
                   )),
             ),
-            // lineBetween(),
-            // Container(
-            //   margin: const EdgeInsets.symmetric(vertical: 15),
-            //   child: const Row(
-            //     children: [
-            //       Padding(
-            //         padding: EdgeInsets.only(right: 10),
-            //         child: Icon(Icons.search),
-            //       ),
-            //       Text(
-            //         "Ninh kieu, Can tho",
-            //         style: TextStyle(color: Color.fromARGB(154, 0, 0, 0)),
-            //       )
-            //     ],
-            //   ),
-            // ),
-            // lineBetween(),
-            // buildListPlace(),
-            buildListPlace()
+            lineBetween(),
+            Container(
+              margin: const EdgeInsets.symmetric(vertical: 15),
+              child: const Row(
+                children: [
+                  Padding(
+                    padding: EdgeInsets.only(right: 10),
+                    child: Icon(Icons.search),
+                  ),
+                  Text(
+                    "Ninh kieu, Can tho",
+                    style: TextStyle(color: Color.fromARGB(154, 0, 0, 0)),
+                  )
+                ],
+              ),
+            ),
+            lineBetween()
           ],
         ),
       ),
@@ -103,68 +93,6 @@ class LocationPage extends GetView<LocationController> {
       height: 2,
       width: double.infinity,
       color: AppColors.gray2,
-    );
-  }
-
-  Widget buildListPlace() {
-    return StreamBuilder<List<Place>>(
-      stream: controller.controllerOut,
-      builder: (context, snapshot) {
-        if (snapshot.data == null || snapshot.data!.isEmpty) {
-          return const Center(child: Text('No data address found'));
-        }
-        final data = snapshot.data;
-
-        return data != null && data.isNotEmpty
-            ? Expanded(
-                child: ListView.builder(
-                    // shrinkWrap: true,
-                    itemCount: data!.length,
-                    itemBuilder: (_, index) {
-                      Place place = data![index];
-                      return ItemPlace(place: place);
-                    }),
-              )
-            : const SizedBox.shrink();
-      },
-    );
-  }
-
-  Widget ItemPlace({required Place place}) {
-    return GestureDetector(
-      onTap: () {
-        controller.onSelectPlace(place);
-      },
-      child: Column(
-        children: [
-          const Divider(
-            color: AppColors.gray2,
-            height: 2.0,
-          ),
-          Container(
-            margin: const EdgeInsets.symmetric(vertical: 15),
-            child: Row(
-              children: [
-                const Padding(
-                  padding: EdgeInsets.only(right: 10),
-                  child: Icon(Icons.search),
-                ),
-                Expanded(
-                  child: Text(
-                    place.displayAddress(),
-                    style: const TextStyle(color: Color.fromARGB(154, 0, 0, 0)),
-                    maxLines: 1,
-                  ),
-                )
-              ],
-            ),
-          ),
-          const Divider(
-            color: AppColors.gray2,
-            height: 2.0,
-          ),
-        ],
-      ),
     );
   }
 }
