2e8d708055ddd53910ca5b0f9f3b314871d52cf6
minnhhttris <ntmtri02@gmail.com>
2024-06-17 10:20:56 +0700

    change user's information

--- lib/features/account_setting/nav/setting_information/presentation/controller/setting_information_controller.dart
+++ lib/features/account_setting/nav/setting_information/presentation/controller/setting_information_controller.dart
@@ -3,55 +3,52 @@ import 'package:find_food/core/data/firebase/firestore_database/firestore_user.d
 import 'package:find_food/core/ui/snackbar/snackbar.dart';
 import 'package:find_food/features/auth/user/domain/use_case/get_user_use_case.dart';
 import 'package:find_food/features/auth/user/model/user_model.dart';
+import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 
 class SettingInformationController extends GetxController {
   final GetuserUseCase _getuserUseCase;
   SettingInformationController(this._getuserUseCase);
   UserModel? user;
+  String? displayName;
+  String? email;
+  String? phoneNumber;
+
+  final formKey = GlobalKey<FormState>();
 
   @override
   void onInit() async {
     user = await _getuserUseCase.getUser();
     super.onInit();
-    getUser();
+    await getUser();
     loadData();
   }
 
   Future<void> loadData() async {
-    update(["fetchDataProfilePage", "fetchUser"]);
+    update(["setting_information_controller", "fetchUserInformation"]);
   }
 
-   Future<void> getUser() async {
+  Future<void> getUser() async {
     final result = await FirestoreUser.getUser(user!.uid!);
     if (result.status == Status.success) {
       user = result.data!;
-      update(["fetchUser"]);
+      update(["fetchUserInformation"]);
     } else {
       SnackbarUtil.show(result.exp?.message ?? "Something went wrong");
     }
   }
 
-  void updateUser() async {
-    final result = await FirestoreUser.updateUser(user!);
-    if (result.status == Status.success) {
-      SnackbarUtil.show("Save changes");
-      update(["fetchUser"]);
-    } else {
-      SnackbarUtil.show(result.exp!.message ?? "something_went_wrong");
+  Future<void> updateUser() async {
+    if (formKey.currentState!.validate()) {
+      formKey.currentState!.save();
+      final result = await FirestoreUser.updateUser(user!);
+      if (result.status == Status.success) {
+        SnackbarUtil.show("Save changes");
+        loadData();
+        update(["fetchUserInformation"]);
+      } else {
+        SnackbarUtil.show(result.exp!.message ?? "Something went wrong");
+      }
     }
   }
-
-  Future<void> updateUserInFirestore() async {
-    final result = await FirestoreUser.updateUser(user!);
-    if (result.status == Status.success) {
-      SnackbarUtil.show("Updated successfully!");
-      loadData();
-      update(["fetchUser"]);
-    } else {
-      SnackbarUtil.show(result.exp?.message ?? "Something went wrong");
-    }
-  }
-
-
 }
